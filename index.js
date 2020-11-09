f3b52fa06159e415cb8a0ad1766d9a4414f0d951
Ilona Badur <Ilona.badur@skola.botkyrka.se>
2020-11-09 14:17:01 +0100

    solve1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Read instructions. And remove this line.
+
+function randomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+const secretNumber = randomInt(100)
+
+console.log(secretNumber);
